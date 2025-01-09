6f6c554effe2dbb9f2c1bc9fd769d33485b85a28
Andriy Semenets <semanser@gmail.com>
2025-01-09 13:24:46 +0100

    test installation

--- Formula/depshub.rb
+++ Formula/depshub.rb
@@ -2,19 +2,19 @@ class Depshub < Formula
   desc "DepsHub is a tool to manage your dependencies"
   homepage "https://github.com/DepsHubHQ/depshub"
   version "0.8.3"
+  license "AGPL-3.0"
   
   on_macos do
     on_arm do
       url "https://github.com/DepsHubHQ/depshub/releases/download/v0.8.3/depshub-darwin-arm64"
-      sha256 "#{%x(sha256sum dist/macos-arm64/depshub-darwin-arm64).split.first}"
     end
     on_intel do
       url "https://github.com/DepsHubHQ/depshub/releases/download/v0.8.3/depshub-darwin-amd64"
-      sha256 "#{%x(sha256sum dist/macos-amd64/depshub-darwin-amd64).split.first}"
     end
   end
   
   def install
+    mv "depshub-darwin-#{Hardware::CPU.arch}", "depshub"
     bin.install "depshub"
   end
 end
